313bbbc61172dd07b68e7f08a4fadc2f56536bcb
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2023-02-23 10:25:54 +1100

    update movie view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+  get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :lists
+  resources :movies
 
   # Defines the root path route ("/")
   # root "articles#index"
