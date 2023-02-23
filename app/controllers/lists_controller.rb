5e60d631b5e73e6e6ac6d261e32458e1d93e7ae2
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2023-02-23 17:13:06 +1100

    add style

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,4 +6,21 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
   end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists_path(@list), :notice => "Successfully created list."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'movies/index'
-  get 'lists/index'
+  # get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :lists
-  resources :movies
 
   # Defines the root path route ("/")
   # root "articles#index"
